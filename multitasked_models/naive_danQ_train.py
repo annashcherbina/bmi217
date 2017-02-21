050458b53233544957ddc8452a23f3264861159b
Andrew <awc8888@gmail.com>
2017-02-20 17:40:10 -0800

    add usability script

--- multitasked_models/naive_danQ_train.py
+++ multitasked_models/naive_danQ_train.py
@@ -1,23 +1,14 @@
 import argparse
+import imp
 import numpy as np
 import h5py
 import scipy.io
 
-np.random.seed(1337) # for reproducibility
-
-import keras;
-from keras.models import Sequential
-from keras.layers.core import Dropout, Reshape, Dense, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.optimizers import Adadelta, SGD, RMSprop;
-from keras.constraints import maxnorm;
-from keras.layers.advanced_activations import PReLU
-from keras.layers.normalization import BatchNormalization
-from keras.regularizers import l1, l2, activity_l1, activity_l2
-from keras.callbacks import ModelCheckpoint, EarlyStopping
+global modelModule
 
 def parseArgs():
     parser = argparse.ArgumentParser()
+    parser.add_argument('model_path')
     parser.add_argument('train_path')
     parser.add_argument('valid_path')
     parser.add_argument('test_path')
@@ -36,43 +27,6 @@ def loadData(args):
     x_test = testmat['X']['sequence']
     y_test = testmat['Y']['output']
     return x_train, y_train, x_valid, y_valid, x_test, y_test
-
-def createModel():
-    print "Creating model..."
-
-    model = Sequential()
-    model.add(Convolution2D(300,4,19,input_shape=(1,4,2000)))
-    model.add(BatchNormalization(mode=0, axis=1))
-    model.add(PReLU())
-    model.add(MaxPooling2D(pool_size=(1,3)))
-
-    model.add(Convolution2D(200,1,11,W_constraint=maxnorm(m=7)))
-    model.add(BatchNormalization(mode=0, axis=1))
-    model.add(PReLU())
-    model.add(MaxPooling2D(pool_size=(1,4)))
-
-    model.add(Convolution2D(200,1,7,W_constraint=maxnorm(m=7)))
-    model.add(BatchNormalization(mode=0, axis=1))
-    model.add(PReLU())
-    model.add(MaxPooling2D(pool_size=(1,4)))
-
-    model.add(Flatten())
-    model.add(Dense(1000,activity_regularizer=activity_l1(0.00001),W_constraint=maxnorm(m=7)))
-    model.add(PReLU())
-    model.add(Dropout(0.3))
-
-    model.add(Dense(1000,activity_regularizer=activity_l1(0.00001),W_constraint=maxnorm(m=7)))
-    model.add(PReLU())
-    model.add(Dropout(0.3))
-
-    model.add(Dense(61))
-    model.add(Activation("sigmoid"))
-
-    adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-    
-    print "Compiling model..."
-    model.compile(loss="binary_crossentropy",optimizer=adam)
-    return model
 
 def fitAndEvaluate(model, x_train, y_train, x_valid, y_valid, x_test, y_test):
     print "Running only one epoch"
@@ -97,9 +51,11 @@ def fitAndEvaluate(model, x_train, y_train, x_valid, y_valid, x_test, y_test):
 
 def main():
     args = parseArgs()
+    modelModule = imp.load_source('module.name', args.model_path)
     x_train, y_train, x_valid, y_valid, x_test, y_test = loadData(args)
-    model = createModel()
+    model = modelModule.createModel()
     fitAndEvaluate(model, x_train, y_train, x_valid, y_valid, x_test, y_test)
 
 if __name__ == "__main__":
     main()
+    
